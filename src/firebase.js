83e325945bcff2e01c326137a1528ec70eb110a0
utube8974 <utube8974@gmail.com>
2026-03-15 12:28:28 +0000

    fix: update firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTTnt_7SL7vzq04wkLhLKeGWKJ7b0g0rU",
+  apiKey: "AIzaSyCTTnt_7Sl7vzq04wkLhlKeGWKJ7bOgOrU",
   authDomain: "watchful-idea-473105-n3.firebaseapp.com",
   projectId: "watchful-idea-473105-n3",
   storageBucket: "watchful-idea-473105-n3.firebasestorage.app",
   messagingSenderId: "29118284465",
-  appId: "1:29118284465:web:51549a65c073e97a8c5890"
+  appId: "1:29118284465:web:51549a65c073e97a8c5890",
+  measurementId: "G-RG1G9RLW15"
 };
 
 // Initialize Firebase
